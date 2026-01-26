01ee5437ce51142e982516e45e4b0f04da14c36d
Binh Khuu <binhkhuu90@gmail.com>
2026-01-26 12:58:52 +0800

    Add tests for handler operation and ensure handler file creation

--- cmd/swagger/commands/generate/pathGenerator_test.go
+++ cmd/swagger/commands/generate/pathGenerator_test.go
@@ -3,8 +3,13 @@ package generate
 import (
 	"binh-swagger/cmd/swagger/commands/internal/spec"
 	"binh-swagger/cmd/swagger/commands/internal/testhelpers"
+	"os"
 	"strings"
 	"testing"
+)
+
+const (
+	fileModeExecutable = 0o755
 )
 
 func Test_HnaldeOperation_ShouldThrowErrorWhenProjectNotSetup(t *testing.T) {
@@ -22,6 +27,7 @@ func Test_HnaldeOperation_ShouldThrowErrorWhenProjectNotSetup(t *testing.T) {
 }
 
 func Test_HandleOperation_ShouldIgnoreOmittedOperations(t *testing.T) {
+	resetProjectStructreForTests()
 	tempDir := t.TempDir()
 	path := &spec.PathSpec{
 		Name: "testPath",
@@ -34,5 +40,52 @@ func Test_HandleOperation_ShouldIgnoreOmittedOperations(t *testing.T) {
 	err = handleOperation("GET", path.Name, path.Get, mockFileHelper)
 	if err != nil {
 		t.Errorf("Expected no error for omitted operation, got: %v", err)
+	}
+}
+
+func Test_HandlerOperation_ShouldCreateHandlerFile(t *testing.T) {
+	resetProjectStructreForTests()
+	tempDir := t.TempDir()
+	path := &spec.PathSpec{
+		Name: "testPath",
+		Get: &spec.Operation{
+			Summary:     "Test GET operation",
+			OperationID: "getTestPath",
+			Produces:    []string{"application/json"},
+			Responses:   map[int]spec.ResponseSpec{},
+		},
+	}
+
+	mockFileHelper := testhelpers.CreateMockFileHelper()
+	if err := SetProjectStructure(tempDir); err != nil {
+		t.Fatalf("Failed to set project structure: %v", err)
+	}
+	testProjectStructure, err := GetProjectStructure()
+	if err != nil {
+		t.Fatalf("Failed to get project structure: %v", err)
+	}
+
+	handlerDir := testProjectStructure["handlers"]
+	if err = os.MkdirAll(handlerDir, fileModeExecutable); err != nil {
+		t.Fatal(err)
+	}
+
+	err = handleOperation("GET", path.Name, path.Get, mockFileHelper)
+	if err != nil {
+		t.Errorf("Expected no error for valid operation, got: %v", err)
+	}
+
+	expectedFilePath := mockFileHelper.GetAbsoluteSanitiseFilePath(
+		tempDir+"/internal/handlers",
+		"testPath_handler.go",
+	)
+
+	_, err = os.Stat(expectedFilePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			t.Errorf("Expected handler file to be created at %s, but file does not exist", expectedFilePath)
+			return
+		}
+		t.Errorf("Expected handler file to be created at %s, but got error: %v", expectedFilePath, err)
 	}
 }
