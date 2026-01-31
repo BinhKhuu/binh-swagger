0ad61bdba1790690c316ea20af5528edecb8fcb9
Binh Khuu <binhkhuu90@gmail.com>
2026-01-31 16:18:25 +0800

    Add tests for orchestrator functionality; implement mock configuration and API setup

---
+++ cmd/swagger/commands/generate/orchestrator_test.go
@@ -0,0 +1,60 @@
+package generate
+
+import (
+	filehelper "binh-swagger/cmd/swagger/commands/adaptor"
+	fileHelperMock "binh-swagger/cmd/swagger/commands/helpers/mocks"
+	"binh-swagger/cmd/swagger/commands/internal/spec"
+	"testing"
+)
+
+type mockConfig struct {
+	fileHelper filehelper.FileHelper
+}
+
+func (m *mockConfig) FileHelper() filehelper.FileHelper {
+	return m.fileHelper
+}
+
+func Test_FromAPIConfig_Success(t *testing.T) {
+	tmp := fileHelperMock.ChangeCWDAndCreateGoModFile(t)
+	config := &mockConfig{
+		fileHelper: &filehelper.DefaultFileHelper{},
+	}
+	apiConfig := createMockAPIConfig(tmp)
+	if err := fromAPIConfig(apiConfig, config); err != nil {
+		t.Fatalf("Error generating from API config: %v", err)
+	}
+}
+
+func createMockAPIConfig(root string) *spec.APIConfig {
+	return &spec.APIConfig{
+		ProjectRoot: root,
+		Version:     "1.0.0",
+		Models: map[string]spec.ModelSpec{
+			"User": {
+				Name: "User",
+				Fields: []spec.FieldSpec{
+					{Name: "ID", Type: "int", JSON: "id"},
+					{Name: "Name", Type: "string", JSON: "name"},
+				},
+			},
+			"Product": {
+				Name: "Product",
+				Fields: []spec.FieldSpec{
+					{Name: "ID", Type: "int", JSON: "id"},
+					{Name: "Title", Type: "string", JSON: "title"},
+					{Name: "Price", Type: "float64", JSON: "price"},
+				},
+			},
+		},
+		Paths: map[string]spec.PathSpec{
+			"/users": {
+				Get: &spec.Operation{
+					Summary:     "Get all users",
+					OperationID: "getUsers",
+					Produces:    []string{"application/json"},
+				},
+			},
+		},
+	}
+}
