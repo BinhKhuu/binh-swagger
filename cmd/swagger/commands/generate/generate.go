aeff754dbaa3233862128a5cf4edcf6a29c4b2fd
Binh Khuu <binhkhuu90@gmail.com>
2026-02-21 20:09:28 +0800

    Update setReturnCode function to include response key for dynamic status code handling

--- cmd/swagger/commands/generate/generate.go
+++ cmd/swagger/commands/generate/generate.go
@@ -144,7 +144,7 @@ func SpecToOperationResponses(cfg spec.Operation, mCdm map[string]*ModelCommand)
 			resCmd.Type = res.Schema.Type
 			resCmd.Description = res.Description
 
-			setReturnCode(res, mCdm, ref, resCmd)
+			setReturnCode(res, mCdm, ref, resCmd, responseKey)
 		}
 		responses[responseKey] = *resCmd
 	}
@@ -157,13 +157,13 @@ func SpecToOperationResponses(cfg spec.Operation, mCdm map[string]*ModelCommand)
 // todo test this
 // coudl combine mCdm and ref into a single parameter since ref is only used to get the model name from mCdm
 // change so it loops through all the possible return types and build one return string contraining all the return paths
-func setReturnCode(res spec.ResponseSpec, mCdm map[string]*ModelCommand, ref string, resCmd *ResponseCommand) {
+func setReturnCode(res spec.ResponseSpec, mCdm map[string]*ModelCommand, ref string, resCmd *ResponseCommand, responseKey int) {
 	var code string
 	switch res.Schema.Type {
 	case "array":
-		code = fmt.Sprintf("c.JSON(200,[]models.%s{})", mCdm[ref].Name)
+		code = fmt.Sprintf("c.JSON(%d,[]models.%s{})", responseKey, mCdm[ref].Name)
 	case "object":
-		code = fmt.Sprintf("c.JSON(200, models.%s{})", mCdm[ref].Name)
+		code = fmt.Sprintf("c.JSON(%d, models.%s{})", responseKey, mCdm[ref].Name)
 	default:
 		code = "// todo add comment on return type, currently only supports array and object types"
 	}
